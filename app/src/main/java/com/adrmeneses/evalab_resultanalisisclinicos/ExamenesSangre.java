a39d02d733744875e6a6713ac48c7c246677e8e6
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-10-14 23:41:47 -0600

    EvaLab- Avance vista Examenes Sangre

---
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/ExamenesSangre.java
@@ -0,0 +1,14 @@
+package com.adrmeneses.evalab_resultanalisisclinicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExamenesSangre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.examenes_sangre);
+    }
+}
--- app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/MenuCategorias.java
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/MenuCategorias.java
@@ -2,7 +2,9 @@ package com.adrmeneses.evalab_resultanalisisclinicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuCategorias extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MenuCategorias extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_categorias);
     }
+
+    // Metodos que crean Intents para lanzar las vistas correspondeintes
+    public void viewExamSangre(View view){
+        Intent lanzar = new Intent(this, ExamenesSangre.class);
+        startActivity(lanzar);
+    }
+
 }
