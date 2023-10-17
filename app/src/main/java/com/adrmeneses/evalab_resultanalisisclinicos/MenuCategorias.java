7e4e7ff68b80540f969146373f6070a69b4ff9f5
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-10-17 13:30:48 -0600

    EvaLab- Vistas de todos los examenes IU

--- app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/MenuCategorias.java
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/MenuCategorias.java
@@ -19,5 +19,25 @@ public class MenuCategorias extends AppCompatActivity {
         Intent lanzar = new Intent(this, ExamenesSangre.class);
         startActivity(lanzar);
     }
+    public void viewExamOrina(View view){
+        Intent lanzar1 = new Intent(this, ExamenesOrina.class);
+        startActivity(lanzar1);
+    }
+    public void viewExamFunHepatica(View view){
+        Intent lanzar2 = new Intent(this, ExamenesFunHepatica.class);
+        startActivity(lanzar2);
+    }
+    public void viewExamTiroides(View view){
+        Intent lanzar3 = new Intent(this, ExamenesTiroides.class);
+        startActivity(lanzar3);
+    }
+    public void viewExamFunRenal(View view){
+        Intent lanzar4 = new Intent(this, ExamenesFunRenal.class);
+        startActivity(lanzar4);
+    }
+    public void viewExamInfecciosa(View view){
+        Intent lanzar5 = new Intent(this, ExamenesInfecciones.class);
+        startActivity(lanzar5);
+    }
 
 }
