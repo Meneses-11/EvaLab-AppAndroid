35d36f475e5e111d0f670eb93dd0903c337b0515
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-10-20 19:15:05 -0600

    EvaLab- Creacion IU Hemograma Completo

--- app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/ExamenesSangre.java
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/ExamenesSangre.java
@@ -2,7 +2,9 @@ package com.adrmeneses.evalab_resultanalisisclinicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExamenesSangre extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ExamenesSangre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.examenes_sangre);
+    }
+
+    public void viewHemogramaCompleto(View view){
+        Intent lanzar = new Intent(this, HemogramaCompleto.class);
+        startActivity(lanzar);
     }
 }
---
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/HemogramaCompleto.java
@@ -0,0 +1,14 @@
+package com.adrmeneses.evalab_resultanalisisclinicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HemogramaCompleto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hemograma_completo);
+    }
+}
