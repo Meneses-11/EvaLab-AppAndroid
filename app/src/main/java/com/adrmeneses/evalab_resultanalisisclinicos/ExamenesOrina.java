1e1ac5ea93ca272b8598d512e5e785c92e0ba9f2
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-10-26 01:02:46 -0600

    EvaLab- IU Examen General de Orina Casi terminado

--- app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/ExamenesOrina.java
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/ExamenesOrina.java
@@ -2,7 +2,9 @@ package com.adrmeneses.evalab_resultanalisisclinicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExamenesOrina extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class ExamenesOrina extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.examenes_orina);
     }
+
+    public void viewExamenGeneralOrina(View view){
+        Intent lanzar = new Intent(this, ExamenGeneralOrina.class);
+        startActivity(lanzar);
+    }
+
 }
