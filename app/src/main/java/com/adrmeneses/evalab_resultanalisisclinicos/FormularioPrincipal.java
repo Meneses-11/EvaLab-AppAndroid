9cbf4a69d21b036491de228fe6cdd14f92144a3c
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-11-07 19:42:10 -0600

    EvaLab- Inicio de la encuesta Principal

---
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/FormularioPrincipal.java
@@ -0,0 +1,14 @@
+package com.adrmeneses.evalab_resultanalisisclinicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formulario_principal);
+    }
+}
--- app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/PaginaCarga.java
+++ app/src/main/java/com/adrmeneses/evalab_resultanalisisclinicos/PaginaCarga.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.adrmeneses.evalab_resultanalisisclinicos.basedatos.MyDBHelper;
+
+import java.io.File;
+
 public class PaginaCarga extends AppCompatActivity {
+    File dbFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +21,25 @@ public class PaginaCarga extends AppCompatActivity {
         //Constante para la duración de la pantalla
         final int DURACION = 2500;
 
+        dbFile = getApplicationContext().getDatabasePath(MyDBHelper.DTABASE_NOMBRE);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent intent = new Intent(PaginaCarga.this, MenuPrincipal.class);
-                startActivity(intent);
-                finish();
+                if(dbFile.exists()){
+                    /*Intent intent = new Intent(PaginaCarga.this, MenuPrincipal.class);
+                    startActivity(intent);
+                    finish();*/
+                    Intent intent = new Intent(PaginaCarga.this, FormularioPrincipal.class);
+                    startActivity(intent);
+                    finish();
+                } else{
+                    Intent intent = new Intent(PaginaCarga.this, MenuPrincipal.class);
+                    startActivity(intent);
+                    finish();
+                }
+
 
             }
         }, DURACION);
